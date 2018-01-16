264d001ba6119565de9de27b43dfadfaabd814ec
Jan Pecinovsky <janpecinovsky@gmail.com>
2018-01-16 19:14:33 +0100

    forgot a comma

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',

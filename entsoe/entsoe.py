007df617d12525e637275f5eb95c5110d59c6734
Jan Pecinovsky <janpecinovsky@gmail.com>
2019-07-08 10:57:03 +0200

    version bump

--- entsoe/entsoe.py
+++ entsoe/entsoe.py
@@ -15,7 +15,7 @@ from .parsers import parse_prices, parse_loads, parse_generation, \
     parse_crossborder_flows, parse_imbalance_prices, parse_unavailabilities
 
 __title__ = "entsoe-py"
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 __author__ = "EnergieID.be"
 __license__ = "MIT"
 

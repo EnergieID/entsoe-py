c63f8a4f69ebd1f6b3ac49dc050354ab56e0831b
jpaduart <35497425+jpaduart@users.noreply.github.com>
2020-01-30 23:18:59 +0100

    Reserves (#31)
    * add 7 days and 1 month resolutions definitions
    
    * Implement query for contracted reserve volumes and reserve prices (FCR,
    aFRR, mFRR, RR)
    
    * Get number of allowed elements from the API response instead of
    hard-coded 200.
    
    * Remove deprecation warnings from pd.concat (sort by default)
    
    * Fixes #24: Correctly handle Consumption (this will report consumption
    from e.g. Hydro Pumped Storage correctly but also affects others such as
    Wind Offshore)
    
    * - Second half of bug fix #24
    - Implement same solution on generation per plant
    
    * Avoid unequal length index errors
    
    * Avoid unequal index length errors
    
    * Catch two more error variants to be considered as NoMatchingDataError
    
    * Define MARKETAGREEMENTTYPE constants for contracted reserves market
    agreement types.
    
    * add docstring for type_marketagreement_type
    
    * add truncate to new reserve query methos to conform with existing
    methods
    
    * explain where the addition comes from
    
    * suggestion by JrtPec

--- entsoe/mappings.py
+++ entsoe/mappings.py
@@ -205,6 +205,15 @@ BSNTYPE = {'A29': 'Already allocated capacity (AAC)',
            'B10': 'Congestion income',
            'B11': 'Production unit'}
 
+MARKETAGREEMENTTYPE = {'A01':'Daily',
+                       'A02':'Weekly',
+                       'A03':'Monthly',
+                       'A04':'Yearly',
+                       'A05':'Total',
+                       'A06':'Long term',
+                       'A07':'Intraday',
+                       'A13':'Hourly'}
+
 DOCUMENTTYPE = {'A09': 'Finalised schedule',
                 'A11': 'Aggregated energy data report',
                 'A25': 'Allocation result document',

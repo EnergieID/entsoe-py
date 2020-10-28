ba5da83aa6b25553a189b7a3193e453283b6ca7a
Jan Pecinovsky <janpecinovsky@gmail.com>
2020-10-28 13:21:47 +0100

    version bump

--- entsoe/entsoe.py
+++ entsoe/entsoe.py
@@ -20,7 +20,7 @@ from .parsers import parse_prices, parse_loads, parse_generation, \
     parse_contracted_reserve, parse_imbalance_prices_zip
 
 __title__ = "entsoe-py"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "EnergieID.be"
 __license__ = "MIT"
 

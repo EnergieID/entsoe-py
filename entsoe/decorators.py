080975f3b4a47bde27bcb44da7165e5d27aa4e1d
Frank Boerman <frank@fboerman.nl>
2021-11-13 16:04:04 +0100

    reorganized file structure

---
+++ entsoe/decorators.py
@@ -0,0 +1,104 @@
+from socket import gaierror
+from time import sleep
+import requests
+from functools import wraps
+from .exceptions import NoMatchingDataError, PaginationError
+import pandas as pd
+import logging
+
+from .misc import year_blocks, day_blocks
+
+
+def retry(func):
+    """Catches connection errors, waits and retries"""
+
+    @wraps(func)
+    def retry_wrapper(*args, **kwargs):
+        self = args[0]
+        error = None
+        for _ in range(self.retry_count):
+            try:
+                result = func(*args, **kwargs)
+            except (requests.ConnectionError, gaierror) as e:
+                error = e
+                print("Connection Error, retrying in {} seconds".format(
+                    self.retry_delay))
+                sleep(self.retry_delay)
+                continue
+            else:
+                return result
+        else:
+            raise error
+
+    return retry_wrapper
+
+
+def paginated(func):
+    """Catches a PaginationError, splits the requested period in two and tries
+    again. Finally it concatenates the results"""
+
+    @wraps(func)
+    def pagination_wrapper(*args, start, end, **kwargs):
+        try:
+            df = func(*args, start=start, end=end, **kwargs)
+        except PaginationError:
+            pivot = start + (end - start) / 2
+            df1 = pagination_wrapper(*args, start=start, end=pivot, **kwargs)
+            df2 = pagination_wrapper(*args, start=pivot, end=end, **kwargs)
+            df = pd.concat([df1, df2])
+        return df
+
+    return pagination_wrapper
+
+
+def year_limited(func):
+    """Deals with calls where you cannot query more than a year, by splitting
+    the call up in blocks per year"""
+
+    @wraps(func)
+    def year_wrapper(*args, start, end, **kwargs):
+        blocks = year_blocks(start, end)
+        frames = []
+        for _start, _end in blocks:
+            try:
+                frame = func(*args, start=_start, end=_end, **kwargs)
+            except NoMatchingDataError:
+                logging.debug(f"NoMatchingDataError: between {_start} and {_end}")
+                frame = None
+            frames.append(frame)
+
+        if sum([f is None for f in frames]) == len(frames):
+            # All the data returned are void
+            raise NoMatchingDataError
+
+        df = pd.concat(frames, sort=True)
+        df = df.loc[~df.index.duplicated(keep='first')]
+        return df
+
+    return year_wrapper
+
+
+def day_limited(func):
+    """Deals with calls where you cannot query more than a year, by splitting
+    the call up in blocks per year"""
+
+    @wraps(func)
+    def day_wrapper(*args, start, end, **kwargs):
+        blocks = day_blocks(start, end)
+        frames = []
+        for _start, _end in blocks:
+            try:
+                frame = func(*args, start=_start, end=_end, **kwargs)
+            except NoMatchingDataError:
+                print(f"NoMatchingDataError: between {_start} and {_end}")
+                frame = None
+            frames.append(frame)
+
+        if sum([f is None for f in frames]) == len(frames):
+            # All the data returned are void
+            raise NoMatchingDataError
+
+        df = pd.concat(frames)
+        return df
+
+    return day_wrapper
--- entsoe/entsoe.py
+++ entsoe/entsoe.py
@@ -1,7 +1,4 @@
 import logging
-from functools import wraps
-from socket import gaierror
-from time import sleep
 from typing import Union, Optional, Dict
 
 import pandas as pd
@@ -13,42 +10,18 @@ from bs4 import BeautifulSoup
 from entsoe.exceptions import InvalidPSRTypeError, InvalidBusinessParameterError
 from .exceptions import NoMatchingDataError, PaginationError
 from .mappings import Area, NEIGHBOURS, lookup_area
-from .misc import year_blocks, day_blocks
 from .parsers import parse_prices, parse_loads, parse_generation, \
     parse_installed_capacity_per_plant, parse_crossborder_flows, \
     parse_unavailabilities, parse_contracted_reserve, parse_imbalance_prices_zip, \
     parse_netpositions, parse_procured_balancing_capacity
+from .decorators import retry, paginated, year_limited, day_limited
 
 __title__ = "entsoe-py"
-__version__ = "0.4"
+__version__ = "0.4.1"
 __author__ = "EnergieID.be"
 __license__ = "MIT"
 
 URL = 'https://transparency.entsoe.eu/api'
-
-
-def retry(func):
-    """Catches connection errors, waits and retries"""
-
-    @wraps(func)
-    def retry_wrapper(*args, **kwargs):
-        self = args[0]
-        error = None
-        for _ in range(self.retry_count):
-            try:
-                result = func(*args, **kwargs)
-            except (requests.ConnectionError, gaierror) as e:
-                error = e
-                print("Connection Error, retrying in {} seconds".format(
-                    self.retry_delay))
-                sleep(self.retry_delay)
-                continue
-            else:
-                return result
-        else:
-            raise error
-
-    return retry_wrapper
 
 
 class EntsoeRawClient:
@@ -928,79 +901,6 @@ class EntsoeRawClient:
             country_code=country_code, start=start, end=end,
             doctype="A80", docstatus='A13')
         return content
-    
-
-
-def paginated(func):
-    """Catches a PaginationError, splits the requested period in two and tries
-    again. Finally it concatenates the results"""
-
-    @wraps(func)
-    def pagination_wrapper(*args, start, end, **kwargs):
-        try:
-            df = func(*args, start=start, end=end, **kwargs)
-        except PaginationError:
-            pivot = start + (end - start) / 2
-            df1 = pagination_wrapper(*args, start=start, end=pivot, **kwargs)
-            df2 = pagination_wrapper(*args, start=pivot, end=end, **kwargs)
-            df = pd.concat([df1, df2])
-        return df
-
-    return pagination_wrapper
-
-
-def year_limited(func):
-    """Deals with calls where you cannot query more than a year, by splitting
-    the call up in blocks per year"""
-
-    @wraps(func)
-    def year_wrapper(*args, start, end, **kwargs):
-        blocks = year_blocks(start, end)
-        frames = []
-        for _start, _end in blocks:
-            try:
-                frame = func(*args, start=_start, end=_end, **kwargs)
-            except NoMatchingDataError:
-                logging.debug(f"NoMatchingDataError: between {_start} and {_end}")
-                frame = None
-            frames.append(frame)
-
-        if sum([f is None for f in frames]) == len(frames):
-            # All the data returned are void
-            raise NoMatchingDataError
-
-        df = pd.concat(frames, sort=True)
-        df = df.loc[~df.index.duplicated(keep='first')]
-        return df
-
-    return year_wrapper
-
-
-def day_limited(func):
-    """Deals with calls where you cannot query more than a year, by splitting
-    the call up in blocks per year"""
-
-    @wraps(func)
-    def day_wrapper(*args, start, end, **kwargs):
-        blocks = day_blocks(start, end)
-        frames = []
-        for _start, _end in blocks:
-            try:
-                frame = func(*args, start=_start, end=_end, **kwargs)
-            except NoMatchingDataError:
-                print(f"NoMatchingDataError: between {_start} and {_end}")
-                frame = None
-            frames.append(frame)
-
-        if sum([f is None for f in frames]) == len(frames):
-            # All the data returned are void
-            raise NoMatchingDataError
-
-        df = pd.concat(frames)
-        return df
-
-    return day_wrapper
-
 
 class EntsoePandasClient(EntsoeRawClient):
     @year_limited

342295fd0a023a9c789f73a29da46f210ac4a07b
Wouter Tromp <woutert@gmail.com>
2021-11-19 19:12:19 +0100

    Firsts tests

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,6 @@ def formatting(session):
 @nox.session(python=['3.7', '3.9'])
 def tests(session):
     """Will execute tests on codebase."""
-    session.install("pytest", "pytest-cov")
+    session.install("pytest", "pytest-cov", "pytest-mock", "requests_mock")
     session.install("-e", ".")
     session.run("pytest", "--cov=entsoe", "--cov-report=html", "tests/")
--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,30 @@
+import pandas as pd
+import pytest
+import requests as r
+import requests_mock
+
 from entsoe import EntsoeRawClient
 
+start = pd.Timestamp("20171201", tz="Europe/Brussels")
+end = pd.Timestamp("20180101", tz="Europe/Brussels")
+country_code = "BE"  # Belgium
+country_code_from = "FR"  # France
+country_code_to = "DE_LU"  # Germany-Luxembourg
+type_marketagreement_type = "A01"
+URL = "https://transparency.entsoe.eu/api"
 
-def test_simple() -> None:
-    assert 1 == 1
+
+def test_invalid_rawclient(mocker):
+    """Will initiate RawClient without API key."""
+    with pytest.raises(TypeError):
+        client = EntsoeRawClient()
+
+
+def test_rawclient_get_exception():
+    """Will test if exception handling is triggered in _base_request."""
+    rm = requests_mock.Mocker()
+
+    with pytest.raises(r.HTTPError):
+        rm.get(URL, status_code=500)
+        client = EntsoeRawClient(api_key="test123")
+        client.query_load(country_code, start, end)

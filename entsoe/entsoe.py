fcf5416fd076879c46de5be35e823ab264c7bdba
Frank Boerman <frank@fboerman.nl>
2022-11-29 14:56:38 +0100

    version bump

--- entsoe/entsoe.py
+++ entsoe/entsoe.py
@@ -22,7 +22,7 @@ import warnings
 warnings.filterwarnings('ignore', category=XMLParsedAsHTMLWarning)
 
 __title__ = "entsoe-py"
-__version__ = "0.5.8"
+__version__ = "0.5.9"
 __author__ = "EnergieID.be, Frank Boerman"
 __license__ = "MIT"
 
